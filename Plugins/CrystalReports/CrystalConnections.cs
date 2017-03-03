9d3fb8924a1d852a24afcfdeb2ba280b0944c1c0
Adam Ellis <aellis@SaberlogicLLC.local>
2017-03-03 04:33:33 -0500

    Closes #7

--- Plugins/CrystalReports/CrystalConnections.cs
+++ Plugins/CrystalReports/CrystalConnections.cs
@@ -4,8 +4,6 @@
     {
         public CrystalConnectionInfo() { }
 
-        public string ConnectionName { get; set; }
-        public string ServerAddress { get; set; }
         public string DatabaseName { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
