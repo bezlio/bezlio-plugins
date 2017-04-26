3b1210bebaf6b0d654f53803b32944a8e0b72e98
Adam Ellis <aellis@saberlogic.com>
2017-04-26 10:49:41 -0400

    Closes #63 Also changed some of the defaults for plugin config files for upcoming BRDB release.

--- Plugins/FileSystem/FileSystemPlugin.cs
+++ Plugins/FileSystem/FileSystemPlugin.cs
@@ -13,6 +13,7 @@ namespace bezlio.rdb.plugins
     {
         public string Context { get; set; }
         public string FileName { get; set; }
+        public byte[] Bytes { get; set; }
     }
     public class FileSystem
     {
@@ -102,6 +103,61 @@ namespace bezlio.rdb.plugins
                 response.Data = JsonConvert.SerializeObject(File.ReadAllBytes(locationPath + request.FileName));
 
                 //WriteDebugLog("Response created");
+            }
+            catch (Exception ex)
+            {
+                response.Error = true;
+                response.ErrorText = Environment.MachineName + ": " + ex.Message;
+            }
+
+            // Return our response
+            return response;
+        }
+
+        public static async Task<RemoteDataBrokerResponse> WriteFile(RemoteDataBrokerRequest rdbRequest)
+        {
+            FileSystemDataModel request = JsonConvert.DeserializeObject<FileSystemDataModel>(rdbRequest.Data);
+
+            // Declare the response object
+            RemoteDataBrokerResponse response = new RemoteDataBrokerResponse();
+            response.Compress = rdbRequest.Compress;
+            response.RequestId = rdbRequest.RequestId;
+            response.DataType = "applicationJSON";
+
+            try
+            {
+                // Settings do not seem to reflect in cleanly, we will read the settings directly
+                string asmPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string cfgPath = asmPath + @"\" + "FileSystem.dll.config";
+                string strLocations = "";
+
+                if (File.Exists(cfgPath))
+                {
+                    // Load in the cfg file
+                    XDocument xConfig = XDocument.Load(cfgPath);
+
+                    // Get the setting for the debug log destination
+                    XElement xLocations = xConfig.Descendants("bezlio.plugins.Properties.Settings").Descendants("setting").Where(a => (string)a.Attribute("name") == "fileSystemLocations").FirstOrDefault();
+                    if (xLocations != null)
+                    {
+                        strLocations = xLocations.Value;
+                    }
+                }
+
+                // Deserialize the values from Settings
+                List<FileLocation> locations = JsonConvert.DeserializeObject<List<FileLocation>>(strLocations);
+
+                // Now pick the location path by the name specified
+                if (locations.Where((l) => l.LocationName.Equals(request.Context)).Count() == 0)
+                {
+                    response.Error = true;
+                    response.ErrorText = "Could not locate a location in the plugin config file with the name " + request.Context;
+                    return response;
+                }
+                string locationPath = locations.Where((l) => l.LocationName.Equals(request.Context)).FirstOrDefault().LocationPath;
+
+                File.WriteAllBytes(locationPath + "/" + request.FileName, request.Bytes);
+                response.Data = JsonConvert.SerializeObject("success");
             }
             catch (Exception ex)
             {
