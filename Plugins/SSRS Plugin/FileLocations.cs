fc4622098fdb6bf72f1e1001474f90dffe057ef3
Bob Schnable <rschnable@saberlogic.com>
2017-06-27 13:25:09 -0400

    Separate merge for quote entry helper methods only

---
+++ Plugins/SSRS Plugin/FileLocations.cs
@@ -0,0 +1,10 @@
+﻿namespace bezlio.rdb.plugins
+{
+    public class FileLocation
+    {
+        public FileLocation() { }
+
+        public string LocationName { get; set; }
+        public string LocationPath { get; set; }
+    }
+}
---
+++ Plugins/SSRS Plugin/SSRSConnection.cs
@@ -0,0 +1,11 @@
+﻿namespace bezlio.rdb.plugins
+{
+    public class SSRSConnectionInfo
+    {
+        public SSRSConnectionInfo() { }
+
+        public string DatabaseName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
