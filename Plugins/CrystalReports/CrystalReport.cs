8a04482709294e5d46dfa4916a97714ee0ca655f
sremington <sremington@saberlogic.com>
2017-07-10 11:48:02 -0400

    Added GetReportDataAs() function to report to return report output as Excel, etc...

--- Plugins/CrystalReports/CrystalReport.cs
+++ Plugins/CrystalReports/CrystalReport.cs
@@ -187,6 +187,166 @@ namespace bezlio.rdb.plugins
             reportDocumentType.GetMethod("Dispose").Invoke(ReportObject, null);
         }
 
+        public byte[] GetReportDataAs(string getAsType)
+        {
+            Type exportOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.ExportOptions");
+            Type diskFileDestinationOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.DiskFileDestinationOptions");
+
+            ConstructorInfo diskFileDestinationOptionsConstructor = diskFileDestinationOptionsType.GetConstructor(Type.EmptyTypes);
+
+            dynamic exportOptions = ReportObject.ExportOptions;
+            dynamic diskFileDestinationOptions = diskFileDestinationOptionsConstructor.Invoke(new object[] { });
+
+            PropertyInfo exportFormatType = exportOptionsType.GetProperty("ExportFormatType");
+
+            diskFileDestinationOptions.DiskFileName = Path.GetTempFileName();
+            exportOptions.ExportDestinationOptions = diskFileDestinationOptions;
+
+            var diskFile = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportDestinationType"), "DiskFile");
+            PropertyInfo exportDestinationType = exportOptionsType.GetProperty("ExportDestinationType");
+            exportDestinationType.SetValue(exportOptions, diskFile, null);
+
+            switch(getAsType)
+            {
+                case "Adobe Acrobat (PDF)":
+                    Type pdfFormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.PdfFormatOptions");
+                    ConstructorInfo pdfFormatOptionsConstructor = pdfFormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var pdfFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "PortableDocFormat");
+                    exportFormatType.SetValue(exportOptions, pdfFormat, null);
+
+                    dynamic pdfFormatOptions = pdfFormatOptionsConstructor.Invoke(new object[] { });
+                    MethodInfo createPdfFormatOptionsMethod = exportOptionsType.GetMethod("CreatePdfFormatOptions");
+                    createPdfFormatOptionsMethod.Invoke(pdfFormatOptions, new object[] { });
+
+                    exportOptions.ExportFormatOptions = pdfFormatOptions;
+                    break;
+                case "Inline HTML 3.2 (HTML)":
+                    Type html32FormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.HTMLFormatOptions");
+                    ConstructorInfo html32FormatOptionsConstructor = html32FormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var html32Format = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "HTML32");
+                    exportFormatType.SetValue(exportOptions, html32Format, null);
+
+                    dynamic html32FormatOptions = html32FormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = html32FormatOptions;
+                    break;
+                case "Inline HTML 4.0 (HTML)":
+                    Type html40FormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.HTMLFormatOptions");
+                    ConstructorInfo html40FormatOptionsConstructor = html40FormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var html40Format = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "HTML40");
+                    exportFormatType.SetValue(exportOptions, html40Format, null);
+
+                    dynamic html40FormatOptions = html40FormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = html40FormatOptions;
+                    break;
+                case "Microsoft Excel 97-2000 (XLS)":
+                    Type excelFormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.ExcelFormatOptions");
+                    ConstructorInfo excelFormatOptionsConstructor = excelFormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var excelFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "Excel");
+                    exportFormatType.SetValue(exportOptions, excelFormat, null);
+
+                    dynamic excelFormatOptions = excelFormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = excelFormatOptions;
+                    break;
+                case "Microsoft Excel 97-2000 - Data Only (XLS)":
+                    Type excelDOFormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.ExcelDataOnlyFormatOptions");
+                    ConstructorInfo excelDOFormatOptionsConstructor = excelDOFormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var excelDOFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "ExcelRecord");
+                    exportFormatType.SetValue(exportOptions, excelDOFormat, null);
+
+                    dynamic excelDOFormatOptions = excelDOFormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = excelDOFormatOptions;
+                    break;
+                case "Microsoft Excel Workbook Data-Only (XLSX)":
+                    Type excelWorkbookFormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.ExcelDataOnlyFormatOptions");
+                    ConstructorInfo excelWorkbookFormatOptionsConstructor = excelWorkbookFormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var excelWorkbookFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "ExcelWorkbook");
+                    exportFormatType.SetValue(exportOptions, excelWorkbookFormat, null);
+
+                    dynamic excelWorkbookFormatOptions = excelWorkbookFormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = excelWorkbookFormatOptions;
+                    break;
+                case "Microsoft Word (DOC)":
+                    Type wordFormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.PdfRtfWordFormatOptions");
+                    ConstructorInfo wordFormatOptionsConstructor = wordFormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var wordFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "WordForWindows");
+                    exportFormatType.SetValue(exportOptions, wordFormat, null);
+
+                    dynamic wordFormatOptions = wordFormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = wordFormatOptions;
+                    break;
+                case "Rich Text Format (RTF)":
+                    Type rtfFormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.EditableRTFExportFormatOptions");
+                    ConstructorInfo rtfFormatOptionsConstructor = rtfFormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var rtfFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "RichText");
+                    exportFormatType.SetValue(exportOptions, rtfFormat, null);
+
+                    dynamic rtfFormatOptions = rtfFormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = rtfFormatOptions;
+                    break;
+                case "Separated Values (CSV)":
+                    Type csvFormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.CharacterSeparatedValuesFormatOptions");
+                    ConstructorInfo csvFormatOptionsConstructor = csvFormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var csvFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "CharacterSeparatedValues");
+                    exportFormatType.SetValue(exportOptions, csvFormat, null);
+
+                    dynamic csvFormatOptions = csvFormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = csvFormatOptions;
+                    break;
+                case "Tab Separated Text (TTX)":
+                    // This export type doesn't work
+                    var tabFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "TabSeparatedText");
+                    exportFormatType.SetValue(exportOptions, tabFormat, null);
+
+                    break;
+                case "Text (TXT)":
+                    Type txtFormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.TextFormatOptions");
+                    ConstructorInfo txtFormatOptionsConstructor = txtFormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var txtFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "Text");
+                    exportFormatType.SetValue(exportOptions, txtFormat, null);
+
+                    dynamic txtFormatOptions = txtFormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = txtFormatOptions;
+                    break;
+                case "Extensible Markup Language (XML)":
+                    Type xmlFormatOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.XmlExportFormatOptions");
+                    ConstructorInfo xmlFormatOptionsConstructor = xmlFormatOptionsType.GetConstructor(Type.EmptyTypes);
+
+                    var xmlFormat = Enum.Parse(SharedAssembly.GetType("CrystalDecisions.Shared.ExportFormatType"), "Xml");
+                    exportFormatType.SetValue(exportOptions, xmlFormat, null);
+
+                    dynamic xmlFormatOptions = xmlFormatOptionsConstructor.Invoke(new object[] { });
+
+                    exportOptions.ExportFormatOptions = xmlFormatOptions;
+                    break;
+                default:
+                    break;
+            }
+
+            this.ReportObject.Export();
+            byte[] b = File.ReadAllBytes(diskFileDestinationOptions.DiskFileName);
+            System.IO.File.Delete(diskFileDestinationOptions.DiskFileName);
+            return b;
+        }
+
         public byte[] GetAsPDF()
         {
             Type exportOptionsType = SharedAssembly.GetType("CrystalDecisions.Shared.ExportOptions");
