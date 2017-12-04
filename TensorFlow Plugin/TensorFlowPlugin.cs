4548145c2ccddf08185288cb97a841df8217a77d
Anna Hodakovskaya <ahodakovskaya@saberlogic.com>
2017-12-04 17:02:34 -0500

    Installed pythonnet 3.5 and added 'TensorFlow Plugin' project, that attempts to load and use the pre-installed tensorflow library.

---
+++ TensorFlow Plugin/TensorFlowPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Python.Runtime;
+
+namespace bezlio.rdb.plugins
+{
+    public class TensorFlowPlugin
+    {
+        public static string TF_HelloWorld()
+        {
+            using (Py.GIL())
+            {
+                dynamic tf = Py.Import("tensorflow");
+                dynamic hello = tf.constant("Hello, TensorFlow!");
+                dynamic sess = tf.Session();
+                return sess.run(hello);
+            }
+        }
+    }
+}
