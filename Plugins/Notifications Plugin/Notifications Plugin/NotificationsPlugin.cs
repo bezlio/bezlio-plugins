62306933a6710e9880d80693510f7ee66050de5d
Joel Hines <jhines@bezl.io>
2018-08-14 12:59:00 -0400

    reading perma token from config

--- Plugins/Notifications Plugin/Notifications Plugin/NotificationsPlugin.cs
+++ Plugins/Notifications Plugin/Notifications Plugin/NotificationsPlugin.cs
@@ -5,32 +5,46 @@ using RestSharp;
 using RestSharp.Authenticators;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace bezlio.rdb.plugins
 {
     public class NotificationsModel
     {
-        public string RegistrationToken { get; set; }
-        public string Credential { get; set; }
+        public string Recpient { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+    
+
+    public class SqlFileLocation
+    {
+        public SqlFileLocation() { }
+
+        public string permaToken { get; set; }
     }
 
-    public class NotificationsPlugin
+
+    public class Notifications
     {
         public static object GetArgs()
         {
 
             NotificationsModel model = new NotificationsModel();
-            model.RegistrationToken = "";
-            model.Credential = "";
-            return model;
+            model.Recpient = "Who gets the notification";
+            model.Title = "Notification Title";
+            model.Body = "Notification Body";
+            return model; 
         }
         public static object sendNotification(RemoteDataBrokerRequest rdbRequest)
         {
-            RESTDataModel request = JsonConvert.DeserializeObject<RESTDataModel>(rdbRequest.Data);
+            var request = JsonConvert.DeserializeObject<dynamic>(rdbRequest.Data);
 
             // Declare the response object
             RemoteDataBrokerResponse response = new RemoteDataBrokerResponse();
@@ -40,13 +54,17 @@ namespace bezlio.rdb.plugins
 
             var client = new RestClient("https://bezlio-notifications-3.azurewebsites.net/send");
             RestRequest restRequest = new RestRequest();
-            restRequest.Method = Method.POST;
+            restRequest.Method = Method.GET;
 
             // Check to see if we need to add a body
-            if (request.Body != null && request.Body != "")
+            if (request != null && request.Body != "")
             {
-                restRequest.AddJsonBody(request.Body);
+                restRequest.AddParameter("recpient", request.Body.Recpient);
+                restRequest.AddParameter("title", request.Body.Title);
+                restRequest.AddParameter("body", request.Body.Body);
             }
+
+            restRequest.AddParameter("key", GetLocations().permaToken);
 
             // Execute
             IRestResponse resp = client.Execute(restRequest);
@@ -55,62 +73,30 @@ namespace bezlio.rdb.plugins
 
             return response;
         }
-
-        public static object addDevice(RemoteDataBrokerRequest rdbRequest)
+        private static SqlFileLocation GetLocations()
         {
-            RESTDataModel request = JsonConvert.DeserializeObject<RESTDataModel>(rdbRequest.Data);
-
-            // Declare the response object
-            RemoteDataBrokerResponse response = new RemoteDataBrokerResponse();
-            response.Compress = rdbRequest.Compress;
-            response.RequestId = rdbRequest.RequestId;
-            response.DataType = "applicationJSON";
-
-            var client = new RestClient("https://bezlio-notifications-3.azurewebsites.net/device");
-            RestRequest restRequest = new RestRequest();
-            restRequest.Method = Method.POST;
-
-            // Check to see if we need to add a body
-            if (request.Body != null && request.Body != "")
+            string asmPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string cfgPath = asmPath + @"\" + "Notifications.dll.config";
+            string strConnections = "";
+            if (File.Exists(cfgPath))
             {
-                restRequest.AddJsonBody(request.Body);
+                // Load in the cfg file
+                XDocument xConfig = XDocument.Load(cfgPath);
+
+                // Get the setting for the debug log destination
+                XElement xConnections = xConfig.Descendants("bezlio.plugins.Properties.Settings").Descendants("setting").Where(a => (string)a.Attribute("name") == "permaLinkToken").FirstOrDefault();
+                if (xConnections != null)
+                {
+                    strConnections = xConnections.Value;
+                }
             }
 
-            // Execute
-            IRestResponse resp = client.Execute(restRequest);
+            var contextLocations = JsonConvert.DeserializeObject<SqlFileLocation>(strConnections);
+            
 
-            response.Data = resp.Content;
-
-            return response;
+            return contextLocations;
         }
 
-        public static object removeDevice(RemoteDataBrokerRequest rdbRequest)
-        {
-            RESTDataModel request = JsonConvert.DeserializeObject<RESTDataModel>(rdbRequest.Data);
 
-            // Declare the response object
-            RemoteDataBrokerResponse response = new RemoteDataBrokerResponse();
-            response.Compress = rdbRequest.Compress;
-            response.RequestId = rdbRequest.RequestId;
-            response.DataType = "applicationJSON";
-
-            var client = new RestClient("https://bezlio-notifications-3.azurewebsites.net/device");
-            RestRequest restRequest = new RestRequest();
-            restRequest.Method = Method.DELETE;
-
-            // Check to see if we need to add a body
-            if (request.Body != null && request.Body != "")
-            {
-                restRequest.AddJsonBody(request.Body);
-            }
-
-            // Execute
-            IRestResponse resp = client.Execute(restRequest);
-
-            response.Data = resp.Content;
-
-            return response;
-        }
-        
     }
 }
